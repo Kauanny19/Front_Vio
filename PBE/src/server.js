7dc2254bf3f7b97ec9c9e8e0aa3fe03c3cf665a6
Kauanny19 <kauannycristina01n@gmail.com>
2024-10-02 14:32:51 -0300

    -

--- PBE/src/server.js
+++
@@ -1,4 +0,0 @@
-const app = require("./index"); //Importar a instacia do Express configurada em index.js
-app.listen(5000); //Inicia servidor na porta 5000, neste caso a API sera acessivel em http://localhost:5000/
-
-// http://localhost:5000/projeto_de_reserva/health/
